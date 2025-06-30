d01fa9977d29a3be25f96fd3a6d69f9920def734
Javieroliver-web <javip200555@gmail.com>
2025-06-30 16:48:14 +0200

    Clase 12 completada

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'Hola Mundo Angular!';
+   title: string = 'Hola Mundo Angular desde componente!';
+
+   users: string[] = ['Javi', 'Juanda', 'Ana', 'Marta' ];
+   
+   visible: boolean = false;
+
+   setVisible(): void {
+    this.visible = this.visible? false: true;
+    console.log('Hemos hecho click en setVisible');
+   }
 }
